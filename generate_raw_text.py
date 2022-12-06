c4416cc2fcb4e6a69477be0af5f891847266b8a1
McGarry <kmcgarry3@elsys.gtri.org>
2022-12-06 16:46:30 -0500

    Updated text generation to include sed contra and respondo dicens

--- generate_raw_text.py
+++ generate_raw_text.py
@@ -12,7 +12,9 @@ flat_dict = df.to_dict()
 # Write all titles and text to a text file
 with open('all_text.txt', 'w') as f:
     for key, value in flat_dict.items():
-        if 'title' in key or 'text' in key:
+        if ('title' in key or 'text' in key
+             or 'counter' in key or 'body' in key
+        ):
             if str(value[0])[0] is '[':
                 line = str(value[0])[2:-2]
             else:
